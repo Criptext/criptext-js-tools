1b1a8279aaf0d7be8e7d17e1c3dfe49e9c1c4a60
Gabriel Aumala <gabrielfima@live.com>
2017-11-23 15:58:49 -0500

    First release 0.2.2

--- src/utils/child_process.js
+++
@@ -1,8 +0,0 @@
-const { spawnSync } = require('child_process');
-
-const spawnInSameTerminal = (executable, args) =>
-  spawnSync(executable, args, { stdio: 'inherit' });
-
-module.exports = {
-  spawnInSameTerminal
-};
