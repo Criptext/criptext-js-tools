838de2527edba0988682fce6308420f6b9d9576b
Gabriel Aumala <gabrielfima@live.com>
2018-10-02 15:04:23 -0500

    Catch syntax errors thrown by prettier

--- src/tools/prettier.js
+++ src/tools/prettier.js
@@ -6,8 +6,15 @@ const getOptionsForFile = filepath => Object.assign({}, config, { filepath });
 const getPrettierSourceCode = (sourceCode, filepath) => {
   const options = getOptionsForFile(filepath);
 
-  if (prettier.check(sourceCode, options)) return null;
-  return prettier.format(sourceCode, options);
+  try {
+    if (prettier.check(sourceCode, options)) return null;
+    return prettier.format(sourceCode, options);
+  } catch (err) {
+    if (!err.codeFrame) throw err;
+
+    console.error(`Found unexpected token while processing file ${filepath}:`);
+    console.error(err.codeFrame);
+  }
 };
 
 module.exports = { getPrettierSourceCode };
