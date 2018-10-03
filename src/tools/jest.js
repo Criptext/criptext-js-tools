c84c022f37a954876ba4d61010c47f31e56785dd
Gabriel Aumala <gabrielfima@live.com>
2018-10-02 20:50:34 -0500

    set appropriate jest node env for tests

--- src/tools/jest.js
+++ src/tools/jest.js
@@ -1,18 +1,21 @@
 const jest = require('jest');
 const defaultConfig = require('../config/jest.config.js');
+const { isFrontendProject } = require('./project-info.js');
 const integrationConfigPath = require.resolve(
   '../config/jest.integration.config.js'
 );
 
 const run = args => {
-  const config = defaultConfig.concat(args);
+  const config = isFrontendProject()
+    ? defaultConfig.concat(args)
+    : defaultConfig.concat(['--env=node', ...args]);
 
   jest.run(config);
 };
 
 const runIntegration = args => {
   const config = ['--config', integrationConfigPath, '--runInBand'].concat(
-    args
+    isFrontendProject() ? args : ['--env=node', ...args]
   );
 
   jest.run(config);
